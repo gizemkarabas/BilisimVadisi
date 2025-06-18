49500bc7842e5cc8d6b4980885a36d83afe339b9
gizem karabaş <gizemkarabas720@gmail.com>
2025-06-18 21:05:29 +0300

    Mail gönderiminde saat farkı sorunu çözüldü

--- MeetinRoomRezervation/Services/Services/MeetingReminderService.cs
+++ MeetinRoomRezervation/Services/Services/MeetingReminderService.cs
@@ -87,8 +87,8 @@ namespace MeetinRoomRezervation.Services
 		{
 			try
 			{
-				var now = DateTime.UtcNow;
-				var reminderTime = now.AddMinutes(5); // 5 dakika sonrası
+				var now = DateTime.UtcNow.AddHours(3);
+				var reminderTime = now.AddHours(3).AddMinutes(5); // 5 dakika sonrası
 
 				// 5 dakika sonra başlayacak toplantıları bul
 				var upcomingMeetings = await _context.Reservations
